51f738c0be6cb691fa1491dc347178f4c1c29db6
Joseph Petitti <jppetitti@wpi.edu>
2019-07-30 15:56:06 +0900

    add bare bones api

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
-const express = require('express');
+const express    = require('express');
+const bodyParser = require('body-parser');
+const mysql      = require('mysql');
 const app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// specify router
+// specify router and other verbs
 require('./router/main')(app);
+require('./router/api')(app);
+
+// use to parse application/json
+app.use(bodyParser.json());
+
+// create database connection
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'restful_db'
+});
+
+// connect to the database
+conn.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database...');
+});
 
 // specify views directory
 app.set('views', __dirname + '/views');
+
+// specify directory for static content
+app.use(express.static(__dirname + '/static'));
 
 app.engine('html', require('ejs').renderFile);
 
