71027fc646d25bf3a71d9b07b58edbde628e7dfc
Joseph Petitti <jppetitti@wpi.edu>
2019-08-08 11:37:47 +0900

    add /api/game/:id API endpoint

---
+++ create-tables.sql
@@ -0,0 +1,51 @@
+/*
+ * Create a database called reversi_db and then run this to create tables in it
+ * Something like this should work:
+ * mysql -h host.example.com -u reversi -p reversi_db < create-tables.sql
+ */
+
+-- drop all tables to start fresh
+-- make sure these are in the right order to delete foreign key holders first
+DROP TABLE IF EXISTS `plays`;
+DROP TABLE IF EXISTS `game`;
+DROP TABLE IF EXISTS `player`;
+
+CREATE TABLE `game` (
+	id       VARCHAR(10) NOT NULL PRIMARY KEY,
+	created  DATETIME NOT NULL,
+	modified DATETIME
+);
+
+CREATE TABLE `player` (
+	id       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name     VARCHAR(20) NOT NULL,
+	password VARCHAR(128) NOT NULL,
+	created  DATETIME NOT NULL
+);
+
+CREATE TABLE `plays` (
+	gID VARCHAR(10) NOT NULL,
+	pID INT NOT NULL
+);
+
+ALTER TABLE plays ADD CONSTRAINT pk_gidpid PRIMARY KEY (gID, pID);
+ALTER TABLE plays ADD CONSTRAINT fk_gid FOREIGN KEY (gID) REFERENCES game(id);
+ALTER TABLE plays ADD CONSTRAINT fk_pid FOREIGN KEY (pID) REFERENCES player(id);
+
+
+/* Test Data */
+
+INSERT INTO game VALUES ('asdf', '2019-02-03 10:52:00', '2019-08-09 01:22:00');
+INSERT INTO game VALUES ('qwerty', '2017-10-27 09:22:00', NULL);
+
+INSERT INTO player (name, password, created) VALUES 
+	('John', 'asldkfjaldskfj', '2019-01-01 10:20:20'),
+	('Paul', 'oianbeine', '2018-09-27 22:30:33'),
+	('George', '0asd9jnbkwj', '2017-02-20 18:33:00'),
+	('Ringo', '9v0n3n113nk3lbv', '2018-01-01 19:30:00');
+
+INSERT INTO plays VALUES ('asdf', 1);
+INSERT INTO plays VALUES ('asdf', 2); 
+INSERT INTO plays VALUES ('asdf', 3);
+INSERT INTO plays VALUES ('qwerty', 4);
+
--- router/api.js
+++ router/api.js
@@ -1,5 +1,38 @@
 module.exports = (app, conn) => {
 
+  // get info about a single game from the database, the last time it was
+  // modified and the names of each of the players
+  app.get('/api/game/:id', (req, res) => {
+    let sql =
+      `SELECT P.name, G.modified
+       FROM player P, plays Z, game G
+       WHERE G.id = '${req.params.id}' AND  Z.gID = G.id AND P.id = Z.pID;`
+    let query = conn.query(sql, (err, results) => {
+      if (err) throw err;
+     
+      if (results.length < 1) {
+        // if no results, send an error
+        res.send(JSON.stringify({
+          'status': 400, 
+          'error': 'Database returned no data. Probably an invalid game ID', 
+          'response': null
+        }));
+      } else {
+        // otherwise make the results a little more readable and send them
+        let players = results.map((r) => r.name);
+        let output = {'players': players, 'modified': results[0].modified};
+        res.send(JSON.stringify(
+          {'status': 200, 'error': null, 'response': output}
+        ));
+      }
+    });
+  });
+
+
+  /*
+   * The rest of these are just examples and won't actually work as-is
+   * They should eventually be removed
+   */
   // show all products
   app.get('/api/products', (req, res) => {
     let sql = 'SELECT * FROM product';
