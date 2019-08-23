d419c932c565543bbe48da5de25daecd1e7e009a
Joseph Petitti <jppetitti@wpi.edu>
2019-08-23 10:07:40 +0900

    finish checkForLine

--- model/Board.js
+++ model/Board.js
@@ -7,25 +7,37 @@ const Player = require('./Player');
 class Board {
   /**
    * Creates a new Board object to keep track of the board state and players.
-   * State is accessed as myBoard.state[column][row]
-   * @param {number} [width=8] the width of the board
+   * State is accessed as myBoard.state[x][y], because it is an array of
+   * columns
+   * @param {number} [width=8] the width of the board, between 2 and 16
    * @param {number} [height=8] the height of the board
    * @param {number} [numPlayers=2] the number of players
    * @constructor
    */
   constructor(width = 8, height = 8, numPlayers = 2) {
+    // check for invalid input
+    if ((width = Math.floor(width)) < 2 || width > 16) {
+      throw new Error('Board constructor: Invalid width: ' + width);
+    }
+    if ((height = Math.floor(height)) < 2 || height > 16) {
+      throw new Error('Board constructor: Invalid height: ' + height);
+    }
+    if ((numPlayers = Math.floor(numPlayers)) < 2) {
+      // TODO set upper limit on numPlayers
+      throw new Error('Board constructor: Invalid numPlayers: ' + numPlayers);
+    }
+
     /** @type number[][] */
-    const state = new Array(width);
+    this.state = new Array(width);
     for (let i = 0; i < width; ++i) {
-      state[i] = new Array(height);
+      this.state[i] = new Array(height);
       for (let j = 0; j < height; ++j) {
-        state[i][j] = 0;
+        this.state[i][j] = 0;
       }
     }
 
     this.width = width;
     this.height = height;
-    this.state = state;
     this.numPlayers = numPlayers;
     this.players = new Array(numPlayers);
     for (let i = 0; i < numPlayers; ++i) {
@@ -61,8 +73,8 @@ class Board {
    */
   stateToString() {
     let out = '';
-    for (let i = 0; i < this.width; ++i) {
-      for (let j = 0; j < this.height; ++j) {
+    for (let j = 0; j < this.height; ++j) {
+      for (let i = 0; i < this.width; ++i) {
         out += this.state[i][j];
       }
       out += '\n';
@@ -89,10 +101,10 @@ class Board {
           for (let dy = -1; dy <= 1; dy++) {
             // check for out of bounds
             if (
-              i + dx > 0 && // not out of bounds
+              i + dx >= 0 && // not out of bounds
               i + dx < this.width &&
-              j + dy > 0 &&
-              j + dy > this.height &&
+              j + dy >= 0 &&
+              j + dy < this.height &&
               this.state[i + dx][j + dy] > 0 && // not empty
               // and not this player's color
               this.state[i + dx][j + dy] !== this.players.indexOf(p) + 1
@@ -113,16 +125,27 @@ class Board {
   }
 
   /**
-   * recursively checks for a line of enemy pieces in a given direction
+   * Checks if placing a piece here would be a valid move for a particular
+   * direction
+   *
+   * x and y are the coordinates of an empty space on the board. dx and dy are
+   * offsets specifying the direction to check in (e.g. dx = -1, dy = 0 means
+   * check to the left). This method recursively walks in the given direction,
+   * looking for an unbroken chain of enemy pieces terminated by a friendly
+   * piece. If it finds this, it returns true, false otherwise.
+   *
    * @param {number} x x-coordinate to check
    * @param {number} y y-coordinate to check
-   * @param {number} dx delta-x: either -1, 0, or 1
-   * @param {number} dy delta-y: either -1, 0, or 1
+   * @param {number} dx delta-x: either -1, 0, or 1. Positive dx goes right
+   * while negative dx goes left
+   * @param {number} dy delta-y: either -1, 0, or 1. Note that positive dy goes
+   * down while negative dy goes up
    * @param {Player} p the player whose turn it is
+   * @param {boolean} [first=true] whether this is the first place we've checked
    * @return {boolean} true if a valid line of enemy pieces terminated by a
    * friendly piece is found, false otherwise
    */
-  checkForLine(x, y, dx, dy, p) {
+  checkForLine(x, y, dx, dy, p, first = true) {
     // check for valid input
     if ((x = Math.floor(x)) < 0 || x > this.width) {
       throw new Error('checkForLine: Invalid x input: ' + x);
@@ -137,11 +160,16 @@ class Board {
       throw new Error('checkForLine: Invalid dy input: ' + dy);
     }
 
+    // if the first space isn't empty it obviously can't be a valid move
+    if (first && this.state[x][y] > 0) {
+      return false;
+    }
+
     if (
-      x + dx > 0 && // not out of bounds
+      x + dx >= 0 && // not out of bounds
       x + dx < this.width &&
-      y + dy > 0 &&
-      y + dy > this.height &&
+      y + dy >= 0 &&
+      y + dy < this.height &&
       this.state[x + dx][y + dy] > 0 // not empty
     ) {
       if (this.state[x + dx][y + dy] === this.players.indexOf(p) + 1) {
@@ -149,7 +177,7 @@ class Board {
       } else {
         // found an enemy piece. Keep walking down the same direction to look
         // for a friendly piece
-        return this.checkForLine(i + dx, j + dy, dx, dy);
+        return this.checkForLine(x + dx, y + dy, dx, dy, p, false);
       }
     } else {
       // reached out of bounds or an empty square
--- test/Board.js
+++ test/Board.js
@@ -3,6 +3,29 @@ const Board = require('../model/Board');
 
 describe('Board', function() {
   describe('#constructor', function() {
+    it('should throw error on invalid input', function() {
+      assert.throws(
+          () => {
+            new Board(-3);
+          },
+          new Error('Board constructor: Invalid width: -3')
+      );
+
+      assert.throws(
+          () => {
+            new Board(4, 45.2);
+          },
+          new Error('Board constructor: Invalid height: 45')
+      );
+
+      assert.throws(
+          () => {
+            new Board(4, 8, 0);
+          },
+          new Error('Board constructor: Invalid numPlayers: 0')
+      );
+    });
+
     it('should use 8, 8, and 2 as the default arguments', function() {
       const defaultBoard = new Board();
       assert.strictEqual(defaultBoard.width, 8);
@@ -38,8 +61,20 @@ describe('Board', function() {
   });
 
   describe('#checkForLine', function() {
+    const testBoard = new Board();
+    const p1 = testBoard.players[0];
+    const p2 = testBoard.players[1];
+    testBoard.state = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 1, 0, 0],
+      [0, 0, 0, 2, 1, 2, 0, 0],
+      [0, 0, 2, 2, 0, 0, 0, 0],
+      [0, 1, 0, 2, 0, 0, 0, 0],
+      [0, 0, 0, 2, 0, 0, 0, 0],
+    ];
     it('should throw error on invalid input', function() {
-      const testBoard = new Board().defaultStart();
       assert.throws(
           () => {
             testBoard.checkForLine(-2, 3, 0, 0);
@@ -66,22 +101,36 @@ describe('Board', function() {
       );
     });
 
-    it('should return false when valid line not found', function() {
-      const testBoard2 = new Board();
-      const p1 = testBoard2.players[0];
-      testBoard2.state = [
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 2, 1, 2, 0, 0],
-        [0, 0, 2, 2, 0, 0, 0, 0],
-        [0, 1, 0, 2, 0, 0, 0, 0],
-        [0, 0, 0, 2, 0, 0, 0, 0],
-      ];
-      assert.strictEqual(testBoard2.checkForLine(3, 3, -1, 0, p1), false);
-      // TODO this needs more testing
+    it('should return false on a non-empty square', function() {
+      assert.strictEqual(testBoard.checkForLine(6, 1, -1, -1, p1), false);
+      assert.strictEqual(testBoard.checkForLine(6, 1, -1, -1, p2), false);
+    });
 
+    it('should return false when no adjacent enemy piece', function() {
+      assert.strictEqual(testBoard.checkForLine(3, 4, 0, -1, p1), false);
+    });
+
+    it('should return false when adjacent friendly piece', function() {
+      assert.strictEqual(testBoard.checkForLine(3, 4, 1, 1, p1), false);
+    });
+
+    it(
+        'should return false when line of enemies reaches board edge',
+        function() {
+          assert.strictEqual(testBoard.checkForLine(3, 4, 1, 0), false);
+        }
+    );
+
+    it('should return true for valid lines', function() {
+      const simpleBoard = new Board(3, 3, 2);
+      const simpleP1 = simpleBoard.players[0];
+      simpleBoard.state = [
+        [0, 2, 1],
+        [0, 2, 0],
+        [0, 1, 0],
+      ];
+      assert.strictEqual(simpleBoard.checkForLine(0, 0, 0, 1, simpleP1), true);
+      assert.strictEqual(testBoard.checkForLine(2, 6, 1, -1, p2), true);
     });
   });
 });
